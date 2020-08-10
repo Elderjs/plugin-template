b59ddd6deac3f4d1178e1b106990e3a19e1bc9f9
Nick Reese <nick@nicholasreese.com>
2020-08-10 11:27:36 -0400

    initial plugin template

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# plugin-template
-Elder.js plugin template
+# Elder.js Plugin Template
+
+If you are looking to write an [Elder.js](https://elderguide.com/tech/elderjs/) plugin we've setup this template to help streamline that process.
+
+Recommended naming is `elderjs-plugin-` + `your-plugin-name`.
+
+Below you'll find recommended minimal documenation. 
+
+Once you've got your plugin written, please drop us a PR to add it to [Elderjs/plugins](https://github.com/Elderjs/plugins) repo. 
+
+--------
+
+
+# Elder.js Plugin: Your Plugin
+
+> Here is where you'd write an intro to your plugin.
+
+
+## Install
+
+> Here is where you'd want to include how to install your plugin.
+
+```bash
+npm i -s elderjs-plugin-your-plugin
+```
+
+
+## Config
+
+> Here is where you want to outline the config options of your plugin.
+
+Once installed, open your `elder.config.js` and configure the plugin by adding 'elder-plugin-your-plugin' to your plugin object.
+
+```javascript
+plugins: {
+
+  'elder-plugin-your-plugin': {
+    ... your plugin options
+  },
+
+}
+```
---
+++ index.js
@@ -0,0 +1,59 @@
+const plugin = {
+  name: 'elder-plugin-your-plugin',
+  description: `[copy and paste the start of your readme]`,
+  init: (plugin) => {
+    // this is a sync function that runs on plugin initialization.
+    // if you need async, it is recommended that you extract the async logic to run on the 'bootstrap' hook.
+
+    // Plugins have their own closure scope. This means that if you set:
+    // plugin.init = true
+    // you will have access to plugin.init in all of your hooks.
+    // this data can be updated in hooks and will be persistent between page loads.
+    
+    // IMPORTANT: It is important to note that since builds are run across child processes, 
+    // the 'plugin' object is not consistent across all processes.
+
+    // Plugins also get the build settings (plugin.settings) and the config (plugin.config) settings. 
+ 
+    return plugin;
+  },
+  hooks: [
+    {
+      hook: 'bootstrap',
+      name: 'yourFirstHook',
+      description: `A description of what this hook does.`,
+      priority: 50,
+      run: async ({ plugin, routes }) => {
+
+        // all props and mutations are detailed here: https://github.com/Elderjs/elderjs/blob/master/src/hookInterface/hookInterface.ts
+        // if you are looking for details on what a prop or mutation represents you can read this: https://github.com/Elderjs/elderjs/blob/master/src/hookInterface/hookEntityDefinitions.ts
+
+        // here is how you'd read the init property set in the init() function
+        if(plugin.init){
+          plugin.bootstrapRan = true;
+          return {
+            plugin,
+          };
+        }
+      }
+    },
+    {
+      hook: 'request',
+      name: 'yourSecondHook',
+      description: `A description of what this hook does.`,
+      priority: 50,
+      run: async ({ plugin, routes }) => {
+        if(plugin.init){
+          return {
+            plugin,
+          };
+        }
+      }
+    },
+  ],
+  config: { // here is where you set the default configs for your plugin. These are merged with the configs found in the user's elder.config.js file.
+    doMagic: true,
+  },
+};
+
+module.exports = plugin;
